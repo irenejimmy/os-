8a83ae018a83921007a31382c6ca06fba540691b
irenejimmy <irenejimmy2028@cs.sjcetpalai.ac.in>
2026-05-27 20:04:11 +0530

    Refactor C-SCAN algorithm and sorting function

--- cscan.c
+++ cscan.c
@@ -1,67 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void sort(int a[], int n) {
-    int i, j, t;
+void sort(int arr[], int n) {
+
+    int i, j, temp;
 
     for(i = 0; i < n - 1; i++) {
+
         for(j = i + 1; j < n; j++) {
-            if(a[i] > a[j]) {
-                t = a[i];
-                a[i] = a[j];
-                a[j] = t;
+
+            if(arr[i] > arr[j]) {
+
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
     }
 }
 
+void cscan(int req[], int n, int head) {
+
+    int left[100], right[100];
+    int l = 0, r = 0;
+    int total = 0, pos, i;
+    int disk_size = 200;
+
+    for(i = 0; i < n; i++) {
+
+        if(req[i] < head)
+            left[l++] = req[i];
+        else
+            right[r++] = req[i];
+    }
+
+    sort(left, l);
+    sort(right, r);
+
+    pos = head;
+
+    printf("\nC-SCAN Seek Sequence: %d", pos);
+
+    for(i = 0; i < r; i++) {
+
+        total += abs(pos - right[i]);
+        pos = right[i];
+
+        printf(" -> %d", pos);
+    }
+
+    total += abs(pos - (disk_size - 1));
+    pos = disk_size - 1;
+
+    printf(" -> %d", pos);
+
+    total += abs(pos - 0);
+    pos = 0;
+
+    printf(" -> %d", pos);
+
+    for(i = 0; i < l; i++) {
+
+        total += abs(pos - left[i]);
+        pos = left[i];
+
+        printf(" -> %d", pos);
+    }
+
+    printf("\nTotal Seek Time = %d\n", total);
+}
+
 int main() {
-    int n, h, r[20], l[20], ri[20];
-    int i, lc = 0, rc = 0;
-    int pos, total = 0;
+
+    int n, head, req[100], i;
 
     printf("Enter number of requests: ");
     scanf("%d", &n);
 
     printf("Enter request sequence:\n");
+
     for(i = 0; i < n; i++)
-        scanf("%d", &r[i]);
+        scanf("%d", &req[i]);
 
     printf("Enter initial head position: ");
-    scanf("%d", &h);
+    scanf("%d", &head);
 
-    for(i = 0; i < n; i++) {
-        if(r[i] < h)
-            l[lc++] = r[i];
-        else
-            ri[rc++] = r[i];
-    }
-
-    sort(l, lc);
-    sort(ri, rc);
-
-    pos = h;
-
-    printf("\nSeek Sequence: %d", pos);
-
-    for(i = 0; i < rc; i++) {
-        total += abs(pos - ri[i]);
-        pos = ri[i];
-        printf(" -> %d", pos);
-    }
-
-    total += abs(pos - 199);
-    pos = 0;
-
-    printf(" -> %d", pos);
-
-    for(i = 0; i < lc; i++) {
-        total += abs(pos - l[i]);
-        pos = l[i];
-        printf(" -> %d", pos);
-    }
-
-    printf("\nTotal Seek Time = %d\n", total);
+    cscan(req, n, head);
 
     return 0;
 }
