e410219727d9b28c57a0d6d39a4d042a115471a9
irenejimmy <irenejimmy2028@cs.sjcetpalai.ac.in>
2026-05-28 09:54:20 +0530

    Refactor optimal page replacement algorithm

--- optimal.c
+++ optimal.c
@@ -1,72 +1,106 @@
 #include <stdio.h>
 
-int main() {
-    int ref[50], frame[10];
-    int n, f, i, j, k, pos, far, hit, faults = 0;
+#define MAX 50
 
-    printf("Enter number of pages: ");
-    scanf("%d", &n);
+void optimal(int pages[], int n, int frames)
+{
+    int frame[MAX];
+    int i, j, k;
+    int faults = 0, hits = 0;
 
-    printf("Enter reference string: ");
-    for(i = 0; i < n; i++)
-        scanf("%d", &ref[i]);
-
-    printf("Enter number of frames: ");
-    scanf("%d", &f);
-
-    for(i = 0; i < f; i++)
+    for(i = 0; i < frames; i++)
         frame[i] = -1;
 
-    printf("\nPage\tFrames\n");
+    printf("\nOptimal Page Replacement:\n");
 
-    for(i = 0; i < n; i++) {
-        hit = 0;
+    for(i = 0; i < n; i++)
+    {
+        int hit = 0;
 
-        for(j = 0; j < f; j++) {
-            if(frame[j] == ref[i]) {
+        for(j = 0; j < frames; j++)
+        {
+            if(frame[j] == pages[i])
+            {
                 hit = 1;
+                hits++;
                 break;
             }
         }
 
-        if(!hit) {
-            pos = -1;
+        if(hit == 0)
+        {
+            int pos = -1, farthest = i + 1;
 
-            for(j = 0; j < f; j++) {
-                if(frame[j] == -1) {
+            for(j = 0; j < frames; j++)
+            {
+                int found = 0;
+
+                for(k = i + 1; k < n; k++)
+                {
+                    if(frame[j] == pages[k])
+                    {
+                        found = 1;
+
+                        if(k > farthest)
+                        {
+                            farthest = k;
+                            pos = j;
+                        }
+
+                        break;
+                    }
+                }
+
+                if(found == 0)
+                {
                     pos = j;
                     break;
                 }
             }
 
-            if(pos == -1) {
-                far = -1;
+            if(pos == -1)
+                pos = 0;
 
-                for(j = 0; j < f; j++) {
-                    for(k = i + 1; k < n; k++) {
-                        if(frame[j] == ref[k])
-                            break;
-                    }
-
-                    if(k > far) {
-                        far = k;
-                        pos = j;
-                    }
-                }
-            }
-
-            frame[pos] = ref[i];
+            frame[pos] = pages[i];
             faults++;
         }
 
-        printf("%d\t", ref[i]);
-        for(j = 0; j < f; j++)
-            printf("%d ", frame[j]);
-        printf("\n");
+        printf("\nPage %d --> ", pages[i]);
+
+        for(j = 0; j < frames; j++)
+        {
+            if(frame[j] == -1)
+                printf("- ");
+            else
+                printf("%d ", frame[j]);
+        }
+
+        if(hit)
+            printf("Hit");
+        else
+            printf("Miss");
     }
 
-    printf("\nPage Faults = %d", faults);
-    printf("\nPage Hits = %d", n - faults);
+    printf("\nTotal Page Faults = %d", faults);
+    printf("\nTotal Page Hits = %d\n", hits);
+}
+
+int main()
+{
+    int pages[MAX], n, frames, i;
+
+    printf("Enter number of pages: ");
+    scanf("%d", &n);
+
+    printf("Enter page sequence:\n");
+
+    for(i = 0; i < n; i++)
+        scanf("%d", &pages[i]);
+
+    printf("Enter number of frames: ");
+   scanf("%d", &frames);
+
+    optimal(pages, n, frames);
 
     return 0;
 }
