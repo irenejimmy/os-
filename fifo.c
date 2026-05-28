6e42cef951848c094d7fddfd0b2716f822788637
irenejimmy <irenejimmy2028@cs.sjcetpalai.ac.in>
2026-05-28 09:52:41 +0530

    Refactor FIFO page replacement logic into a function

--- fifo.c
+++ fifo.c
@@ -1,48 +1,72 @@
 #include <stdio.h>
+#define MAX 50
 
-int main() {
-    int ref[50], frame[10];
-    int n, f, i, j, k = 0, hit, faults = 0;
+void fifo(int page[], int n, int frames)
+{
+    int frame[MAX], index = 0;
+    int i, j, hit, faults = 0, hits = 0;
 
-    printf("Enter number of pages: ");
-    scanf("%d", &n);
-
-    printf("Enter reference string: ");
-    for(i = 0; i < n; i++)
-        scanf("%d", &ref[i]);
-
-    printf("Enter number of frames: ");
-    scanf("%d", &f);
-
-    for(i = 0; i < f; i++)
+    for(i = 0; i < frames; i++)
         frame[i] = -1;
 
-    printf("\nPage\tFrames\n");
+    printf("\nFIFO Page Replacement:\n");
 
-    for(i = 0; i < n; i++) {
+    for(i = 0; i < n; i++)
+    {
         hit = 0;
 
-        for(j = 0; j < f; j++) {
-            if(frame[j] == ref[i]) {
+        for(j = 0; j < frames; j++)
+        {
+            if(frame[j] == page[i])
+            {
                 hit = 1;
+                hits++;
                 break;
             }
         }
 
-        if(!hit) {
-            frame[k] = ref[i];
-            k = (k + 1) % f;
+        if(hit == 0)
+        {
+            frame[index] = page[i];
+            index = (index + 1) % frames;
             faults++;
         }
 
-        printf("%d\t", ref[i]);
-        for(j = 0; j < f; j++)
-            printf("%d ", frame[j]);
-        printf("\n");
+        printf("\nPage %d --> ", page[i]);
+
+        for(j = 0; j < frames; j++)
+        {
+            if(frame[j] == -1)
+                printf("- ");
+            else
+                printf("%d ", frame[j]);
+        }
+
+        if(hit)
+            printf("Hit");
+        else
+            printf("Miss");
     }
 
-    printf("\nPage Faults = %d", faults);
-    printf("\nPage Hits = %d", n - faults);
+    printf("\nTotal Page Faults = %d", faults);
+    printf("\nTotal Page Hits = %d\n", hits);
+}
+
+int main()
+{
+    int pages[MAX], n, frames, i;
+
+    printf("Enter number of pages: ");
+    scanf("%d", &n);
+
+    printf("Enter page sequence:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &pages[i]);
+
+    printf("Enter number of frames: ");
+    scanf("%d", &frames);
+
+    fifo(pages, n, frames);
 
     return 0;
 }
