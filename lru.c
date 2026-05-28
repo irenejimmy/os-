b2b376252e1aa35c7d5743b313ed5fda255dec2a
irenejimmy <irenejimmy2028@cs.sjcetpalai.ac.in>
2026-05-28 09:53:52 +0530

    Refactor LRU page replacement algorithm
    Refactored the LRU page replacement algorithm to improve structure and readability. Moved the logic into a separate function and updated variable names for clarity.

--- lru.c
+++ lru.c
@@ -1,62 +1,88 @@
 #include <stdio.h>
+#define MAX 50
 
-int main() {
-    int ref[50], frame[10], time[10];
-    int n, f, i, j, pos = 0, min, cnt = 0;
-    int hit, faults = 0;
+void lru(int pages[], int n, int frames)
+{
+    int frame[MAX], time[MAX];
+    int i, j, counter = 0;
+    int faults = 0, hits = 0;
 
-    printf("Enter number of pages: ");
-    scanf("%d", &n);
-
-    printf("Enter reference string: ");
-    for(i = 0; i < n; i++)
-        scanf("%d", &ref[i]);
-
-    printf("Enter number of frames: ");
-    scanf("%d", &f);
-
-    for(i = 0; i < f; i++) {
+    for(i = 0; i < frames; i++)
+    {
         frame[i] = -1;
         time[i] = 0;
     }
 
-    printf("\nPage\tFrames\n");
+    printf("\nLRU Page Replacement:\n");
 
-    for(i = 0; i < n; i++) {
-        hit = 0;
+    for(i = 0; i < n; i++)
+    {
+        int hit = 0;
 
-        for(j = 0; j < f; j++) {
-            if(frame[j] == ref[i]) {
+        for(j = 0; j < frames; j++)
+        {
+            if(frame[j] == pages[i])
+            {
                 hit = 1;
-                time[j] = ++cnt;
+                hits++;
+                counter++;
+                time[j] = counter;
                 break;
             }
         }
 
-        if(!hit) {
-            min = time[0];
-            pos = 0;
+        if(hit == 0)
+        {
+            int pos = 0;
 
-            for(j = 1; j < f; j++) {
-                if(time[j] < min) {
-                    min = time[j];
+            for(j = 1; j < frames; j++)
+            {
+                if(time[j] < time[pos])
                     pos = j;
-                }
             }
 
-            frame[pos] = ref[i];
-            time[pos] = ++cnt;
+            frame[pos] = pages[i];
+            counter++;
+            time[pos] = counter;
             faults++;
         }
 
-        printf("%d\t", ref[i]);
-        for(j = 0; j < f; j++)
-            printf("%d ", frame[j]);
-        printf("\n");
+        printf("\nPage %d --> ", pages[i]);
+
+        for(j = 0; j < frames; j++)
+        {
+            if(frame[j] == -1)
+                printf("- ");
+            else
+                printf("%d ", frame[j]);
+        }
+
+        if(hit)
+            printf("Hit");
+        else
+            printf("Miss");
     }
 
-    printf("\nPage Faults = %d", faults);
-    printf("\nPage Hits = %d", n - faults);
+    printf("\nTotal Page Faults = %d", faults);
+    printf("\nTotal Page Hits = %d\n", hits);
+}
+
+int main()
+{
+    int pages[MAX], n, frames, i;
+
+    printf("Enter number of pages: ");
+    scanf("%d", &n);
+
+    printf("Enter page sequence:\n");
+
+    for(i = 0; i < n; i++)
+        scanf("%d", &pages[i]);
+
+    printf("Enter number of frames: ");
+    scanf("%d", &frames);
+
+    lru(pages, n, frames);
 
     return 0;
 }
